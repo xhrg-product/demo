39d2c22446290b5945973984026e5feb13c9d6a5
xhrg <634789257@qq.com>
2024-07-30 11:18:10 +0800

    config log4j2

--- spring-boot-layout/src/main/java/com/github/xhrg/layout/LayoutApplication.java
+++ spring-boot-layout/src/main/java/com/github/xhrg/layout/LayoutApplication.java
@@ -1,13 +1,17 @@
 package com.github.xhrg.layout;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LayoutApplication {
+	
+	private static Logger log = LoggerFactory.getLogger(LayoutApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LayoutApplication.class, args);
+		log.info("123");
 	}
-
 }
