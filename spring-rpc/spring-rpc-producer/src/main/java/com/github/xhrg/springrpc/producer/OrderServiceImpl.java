731dde2d2d9c88aeec91a2de4637dd62750f4384
xhrg <634789257@qq.com>
2024-09-03 13:26:39 +0800

    1

--- spring-rpc/spring-rpc-producer/src/main/java/com/github/xhrg/springrpc/producer/OrderServiceImpl.java
+++ spring-rpc/spring-rpc-producer/src/main/java/com/github/xhrg/springrpc/producer/OrderServiceImpl.java
@@ -1,19 +1,19 @@
 package com.github.xhrg.springrpc.producer;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.xhrg.springrpc.client.OrderService;
 
 /**
- * spring-rpc 其实并不要求接口和实现存在代码code级别的 implements，只要http暴露接口和提供给业务的 FeignClient.java注解 接口能对应上即可。
+ * spring-rpc 其实并不要求接口和实现存在代码code级别的 implements，只要http暴露接口和提供给业务的
+ * FeignClient.java注解 接口能对应上即可。
  */
 @RestController
 public class OrderServiceImpl implements OrderService {
 
     @GetMapping("/queryOrder")
     public String queryOrder() {
-        return  "这是server的追加";
+        return "这是server的追加";
     }
 }
