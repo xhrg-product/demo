5ae50b4ad23a0dde40d2ab87d9a163a1e79b1efd
xhrg <634789257@qq.com>
2024-09-06 09:54:12 +0800

    改名

--- spring-boot-dubbo/spring-boot-dubbo-producer/src/main/java/com/github/xhrg/springrpc/producer/ProviderApplication.java
+++ spring-boot-dubbo/spring-boot-dubbo-producer/src/main/java/com/github/xhrg/springrpc/producer/ProviderApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableDubbo  用了xxx-start 就不需要 enablexxx了
-public class ProducerApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProducerApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
